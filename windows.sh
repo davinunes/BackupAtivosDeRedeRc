0e03b887ba25a65a18e0658e0c44ae15ab368973
Davi <davi.nunes@gmail.com>
2022-01-14 13:57:56 -0300

    Versionando

--- windows.sh
+++ windows.sh
@@ -1,2 +1,3 @@
+# cmd /c ""C:\Program Files\Git\bin\sh.exe" --login -i -- "E:\BackupAtivosDeRedeRc\windows.sh"
 find .git/ -name "desktop.ini" | xargs rm -rf
 cd /e/BackupAtivosDeRedeRc && git pull && git add . && git commit -m "Versionando" && git push origin master
